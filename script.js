393b285253726ea484f8cca8356416f91ab421a2
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-16 23:41:30 +0000

    Added some few things.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.getElementById('button').addEventListener('click', displayMessage)
 
 function displayMessage () {
-  document.getElementById('paragraph').innerHTML = "Hello"
+  document.getElementById('paragraph').innerHTML = "How are you doing today"
+}
+
+document.body.style.backgroundColor = "burlywood";
+
+function myFunction() {
+  var x = document.getElementById("myText").name;
+  document.getElementById("demo").innerHTML = x;
 }
