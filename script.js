b6abcb75c04bdcd521687ec16420ac491271c351
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-17 00:04:20 +0000

    fixed errors.

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 document.getElementById('button').addEventListener('click', displayMessage)
 
 function displayMessage () {
-  document.getElementById('paragraph').innerHTML = "How are you doing today"
+  document.getElementById('paragraph').innerHTML = 'How are you doing today'
 }
 
-document.body.style.backgroundColor = "burlywood";
+document.body.style.backgroundColor ='burlywood';;
 
-function myFunction() {
-  var x = document.getElementById("myText").name;
-  document.getElementById("demo").innerHTML = x;
-}
